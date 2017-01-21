5379533328148f4a6b15d9f21dbc9c41eb3fa70d
Yusuf Çalıkan - Linux <yusufocaliskan@gmail.com>
2017-01-22 01:48:57 +0300

    MP3 Özelliği eklendi

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import youtube_dl
 
 link 	= raw_input('Video Linki: ')
-
+mp3 	= raw_input('Mp3? yes/no:')
 
 class RuttoDownloader:
 
@@ -10,12 +13,37 @@ class RuttoDownloader:
 
 	def download(self, link):
 
-		ydl_opts = {}
+		if mp3 == 'yes':
+
+			ydl_opts = {
+			    'format': 'bestaudio/best',
+			    'postprocessors': [{
+			        'key': 'FFmpegExtractAudio',
+			        'preferredcodec': 'mp3',
+			        'preferredquality': '192',
+			    }]
+			}
+		else:
+			ydl_opts = {}
 
 		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 		    ydl.download([link])
 		
 
+class Logger:
+
+	def debug(self, msg):
+		pass
+
+	def warning(self, msg):
+		pass
+
+	def error(self, msg):
+		pass
+
+	def hook(self,d):
+		if d['status'] == 'finished':
+			print('Done downloading, now converting ...')		
 
 Rutto = RuttoDownloader()
 Rutto.download(link);
