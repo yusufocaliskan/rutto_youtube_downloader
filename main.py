9c71d01e2c3fea411a332bd71cbd928e816b3fc8
Yusuf Çalıkan - Linux <yusufocaliskan@gmail.com>
2017-01-22 01:22:12 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import youtube_dl
+
+link 	= raw_input('Video Linki: ')
+
+
+class RuttoDownloader:
+
+	def __init__(self):
+		pass
+
+	def download(self, link):
+
+		ydl_opts = {}
+
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		    ydl.download([link])
+		
+
+
+Rutto = RuttoDownloader()
+Rutto.download(link);
